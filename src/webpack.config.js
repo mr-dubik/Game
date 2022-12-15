815dbaabc37070dc30de3378222bc9754d284aea
mr-dubik <mr.dubik.v@gmail.com>
2022-12-15 16:50:21 +0300

    bundle

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,5 +1,12 @@
+const path = require("path");
+
 module.exports = {
     entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true,
+    },
     module: {
         rules: [
             { test: /\.css$/, use: ["style-loader", "css-loader"] },
