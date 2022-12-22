8fb9729ae470f896d53ef5579c0f9ee926fce811
mr-dubik <mr.dubik.v@gmail.com>
2022-12-22 11:48:57 +0300

    Исправлена ошибка при вводе команды npm run lint:ts

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
                 exclude: /node_modules/,
             },
             {
-                test: /\.scss$/,
+                test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
