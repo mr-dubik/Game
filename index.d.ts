6405037739bb0ff1ad839f551c7820892683934f
mr-dubik <119943910+mr-dubik@users.noreply.github.com>
2022-12-24 13:25:12 +0300

    JS файлы изменены на TS

--- index.d.ts
+++ index.d.ts
@@ -6,4 +6,13 @@ export {};
 
 declare global {
     interface Window { application: any; }
+    interface Element { 
+        checked: any; 
+        src: any;
+        cardsEasy: any[];
+    }
+    interface EventTarget {
+        src: any;
+        classList: any;
+    }
 }
