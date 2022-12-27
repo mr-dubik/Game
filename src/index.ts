2b4c83ba176648197196291cb215e86570d2e09c
mr-dubik <mr.dubik.v@gmail.com>
2022-12-27 08:15:55 +0300

    внесены изменения в index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import 'styles/style.scss';
+import "./style.scss";
+import "./index.d";
 
-
-import './script';
-import './firstPage';
-import './secondPage';
-
+import "./script";
+import "./firstPage";
+import "./secondPage";
 
 window.application.blocks.FirstPage();
